9b8c03214f383318b905a6a17fec2a6e99abc37b
Alfex-1 <alexandrebrunet@free.fr>
2025-04-08 14:32:38 +0200

    update

--- src/app/_main_.py
+++ src/app/_main_.py
@@ -1294,6 +1294,8 @@ if valid_mod:
     X = df.drop(columns=target)
     y = df[target]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=True)
+    # X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size + val_size, shuffle=True, random_state=42, stratify=y)
+    # X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=test_size / (test_size + val_size), shuffle=True)
 
     # 2. Choisir le meilleur modèle
     results = []
