39c86366c882d4587b3dabf5c8aee2942eb29463
Alfex-1 <alexandrebrunet@free.fr>
2025-04-29 16:28:52 +0200

    update

--- src/app/_main_.py
+++ src/app/_main_.py
@@ -1611,7 +1611,7 @@ if valid_mod:
                                                             X_train=X_train, y_train=y_train,
                                                             cv=cv, scoring=scoring_comp,
                                                             multi_class=multi_class,
-                                                            n_trial=n_trial, n_jobs=-1)
+                                                            n_trials=n_trial, n_jobs=-1)
         
         # Ajouter les résultats à la liste
         results.append({
